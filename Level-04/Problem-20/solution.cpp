2b568d0bac101b5de3fe0d61e49eed44cf41d127
Ali Moustafa <74011755+ali-moustafa-ali@users.noreply.github.com>
2026-01-24 20:17:12 +0200

    Remove all comments

---
+++ Level-04/Problem-20/solution.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+float ReadSquareSide(float& side)
+{
+    cout << "Enter square side: ";
+    cin >> side;
+
+    return side;
+}
+
+
+float CircleAreaInscribedInSquare(float A)
+{
+    
+
+    float Area = (M_PI * pow(A, 2)) / 4;
+
+    return Area;
+}
+
+void PrintResult(float Area)
+{
+    cout << "\nCircle Area = " << Area << endl;
+}
+
+int main()
+{
+    float side;
+    side = ReadSquareSide(side);
+    PrintResult(CircleAreaInscribedInSquare(side));
+
+    return 0;
+}
